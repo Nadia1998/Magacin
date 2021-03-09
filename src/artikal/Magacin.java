f32f9d1f6605fbbf8433941c69aebb679ca7f904
Nadia1998 <nadjacoguric998@gmail.com>
2021-03-09 02:14:28 +0100

    Implementacija metode dodajArtikal

--- src/artikal/Magacin.java
+++ src/artikal/Magacin.java
@@ -1,24 +1,35 @@
 package artikal;
 
+import java.util.LinkedList;
+
 import interfaceMagacin.InterfaceMagacin;
 
 public class Magacin implements InterfaceMagacin {
 
+	public LinkedList<Artikal> artikli = new LinkedList<>();
+
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		
-
+		if (artikal != null && !artikli.contains(artikal)) {
+			artikli.add(artikal);
+			
+		}
+		if (artikal != null) {
+			for (Artikal a : artikli) {
+				if (a.equals(artikal))
+					a.setKolicina(a.getKolicina() + artikal.getKolicina());
+			}
+		}
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		
 
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		
+
 		return null;
 	}
 
