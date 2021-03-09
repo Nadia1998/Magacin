c146eb06eedc231efe1af37421aa07d472207155
Nadia1998 <nadjacoguric998@gmail.com>
2021-03-09 01:45:29 +0100

    Dodata je klasa Magacin koja implementira InterfaceMagacin

---
+++ src/artikal/Magacin.java
@@ -0,0 +1,25 @@
+package artikal;
+
+import interfaceMagacin.InterfaceMagacin;
+
+public class Magacin implements InterfaceMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		
+		return null;
+	}
+
+}
